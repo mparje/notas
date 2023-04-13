c21aaab214d1891a86f9fd34ca7d93ed34a65eca
mparje <126131806+mparje@users.noreply.github.com>
2023-04-12 21:39:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,38 +3,41 @@ import speech_recognition as sr
 import openai
 import os
 
-# Configure la API de OpenAI con su clave secreta
-openai.api_key = os.getenv("YOUR_API_KEY")
+# Configura las credenciales de autenticación para la API de GPT
+openai.api_key = os.getenv("API_KEY")
 
-st.set_page_config(page_title="Transcripción y depuración de audio", page_icon=":microphone:", layout="wide")
-
+# Configura el título de la aplicación
 st.title("Transcripción y depuración de audio")
 
-# Cargar archivo de audio
-audio_file = st.file_uploader("Cargar archivo de audio", type=["wav", "mp3"])
+# Permite al usuario subir un archivo de audio
+archivo = st.file_uploader("Sube un archivo de audio en formato WAV o FLAC", type=["wav", "flac"])
 
-if audio_file:
-    # Convertir el archivo de audio a texto utilizando la biblioteca SpeechRecognition
+if archivo:
+    # Crea un objeto de reconocimiento de voz
     r = sr.Recognizer()
-    audio_data = sr.AudioFile(audio_file)
-    with audio_data as audio:
-        audio_text = r.record(audio)
-        transcript = r.recognize_google(audio_text, language="es-ES")
-        
-    # Mostrar el texto transcrito
-    st.header("Texto transcrito")
-    st.write(transcript)
 
-    # Depurar el texto utilizando la API de OpenAI
-    st.header("Texto depurado")
-    response = openai.Completion.create(
+    # Lee el audio desde el archivo
+    audio = sr.AudioFile(archivo)
+    with audio as source:
+        audio_data = r.record(source)
+
+    # Transcribe el audio utilizando la API de reconocimiento de voz de Google
+    texto = r.recognize_google(audio_data, language='es-ES')
+
+    # Depura el texto transrito utilizando GPT
+    prompt = "Actúa como un secretario que transcribe y redacta en forma ordenada y precisa los pensamientos desordenados del usuario:\n\n" + texto
+    respuesta = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"Actúa como un secretario que transcribe y redacta en forma ordenada y precisa los pensamientos desordenados del usuario:\n{transcript}",
-        temperature=0.7,
-        max_tokens=60,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
     )
-    edited_text = response.choices[0].text
-    st.write(edited_text)
+    texto_dep = respuesta.choices[0].text.strip()
+
+    # Muestra el texto transcrito y depurado en la aplicación
+    st.write("Texto transcrito:")
+    st.write(texto)
+    st.write("Texto depurado:")
+    st.write(texto_dep)
