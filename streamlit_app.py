e2cf40e86c28388fd5a9b94405df2c884d6e948f
mparje <126131806+mparje@users.noreply.github.com>
2023-04-13 17:43:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,62 @@
+import requests
 import streamlit as st
-import speech_recognition as sr
 import openai
 import os
 
-openai.api_key = os.getenv("api_key")
+openai.api_key = os.getenv("YOUR_API_KEY")
 
-# Función para transcribir el audio
-def transcribir_audio(audio_file):
-    recognizer = sr.Recognizer()
-    with sr.AudioFile(audio_file) as source:
-        audio_data = recognizer.record(source)
-        try:
-            texto = recognizer.recognize_google(audio_data, language="es-ES")
-            return texto
-        except sr.UnknownValueError:
-            st.error("No se pudo entender el audio")
-        except sr.RequestError as e:
-            st.error(f"Error al solicitar resultados; {e}")
-
-# Función para mejorar el texto utilizando GPT-3
-def mejorar_texto(texto):
+# función para mejorar el texto generado
+def mejorar_texto(texto_generado):
+    # completar la oración del usuario para obtener un texto coherente
+    prompt = "Completa la siguiente oración: \"" + texto_generado + "\" "
     response = openai.Completion.create(
       engine="text-davinci-003",
-      prompt=(f"Mejora el siguiente texto:\n{texto}\n\n"),
+      prompt=prompt,
       temperature=0.5,
-      max_tokens=150,
-      n = 1,
+      max_tokens=60,
+      n=1,
       stop=None,
-      frequency_penalty=0,
-      presence_penalty=0
-    )
-    return response.choices[0].text.strip()
+      timeout=15,
+      )
+    # obtener el texto mejorado generado por el modelo de lenguaje
+    texto_mejorado = response.choices[0].text.strip()
+    return texto_mejorado
 
-# Interfaz de usuario con Streamlit
-st.title("Transcriptor de notas de voz en español")
+# título de la aplicación
+st.title('Transcripción y mejora de texto')
 
-uploaded_file = st.file_uploader("Sube tu archivo de audio", type=["wav", "mp3"])
+# obtener la URL del audio a transcribir
+url_audio = st.text_input('Ingrese la URL del audio a transcribir:')
 
-if uploaded_file is not None:
-    st.audio(uploaded_file, format="audio/wav")
-    texto_transcrito = transcribir_audio(uploaded_file)
-    st.write("Texto transcrito:")
-    st.write(texto_transcrito)
-    texto_mejorado = mejorar_texto(texto_transcrito)
-    st.write("Texto mejorado:")
-    st.write(texto_mejorado)
+# transcribir el audio utilizando el servicio de reconocimiento de voz de Rev.ai
+if st.button('Transcribir'):
+    headers = {
+        'Authorization': 'Bearer 02l5R8_G98YSnNk_J5iQXhuVMMVo6cKcraDdGz-XrGRKGZUVlSO1PLmst2krtbrprwhZlLSsYhqK4jAhs8XIYQTUShtdY',
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'source_config': {
+            'url': url_audio
+        },
+        'metadata': 'Audio de prueba'
+    }
+    response = requests.post('https://api.rev.ai/speechtotext/v1/jobs', headers=headers, json=data)
+
+    if response.status_code == 200:
+        # obtener el resultado de la transcripción del audio
+        resultado = response.json()
+
+        # obtener el texto generado por el servicio de reconocimiento de voz
+        texto_generado = resultado['transcript']
+
+        # mejorar el texto generado utilizando la función mejorado_texto()
+        texto_mejorado = mejorar_texto(texto_generado)
+
+        # mostrar el texto generado y el texto mejorado en la interfaz de usuario
+        st.subheader('Texto generado:')
+        st.write(texto_generado)
+        st.subheader('Texto mejorado:')
+        st.write(texto_mejorado)
+
+    else:
+        st.error('¡Ocurrió un error al obtener el resultado de la transcripción del audio!') 
