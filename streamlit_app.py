f459345e5b87095f52fdcbe5b331046a2abf2950
mparje <126131806+mparje@users.noreply.github.com>
2023-04-12 23:10:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import speech_recognition as sr
-from tempfile import NamedTemporaryFile
+import openai_secret_manager
+import openai
+import os
 
+openai.api_key = os.getenv("api_key")
 
+# Función para transcribir el audio
 def transcribir_audio(audio_file):
     recognizer = sr.Recognizer()
     with sr.AudioFile(audio_file) as source:
@@ -15,16 +19,30 @@ def transcribir_audio(audio_file):
         except sr.RequestError as e:
             st.error(f"Error al solicitar resultados; {e}")
 
+# Función para mejorar el texto utilizando GPT-3
+def mejorar_texto(texto):
+    response = openai.Completion.create(
+      engine="davinci",
+      prompt=(f"Mejora el siguiente texto:\n{texto}\n\n"),
+      temperature=0.5,
+      max_tokens=150,
+      n = 1,
+      stop=None,
+      frequency_penalty=0,
+      presence_penalty=0
+    )
+    return response.choices[0].text.strip()
 
+# Interfaz de usuario con Streamlit
 st.title("Transcriptor de notas de voz en español")
 
 uploaded_file = st.file_uploader("Sube tu archivo de audio", type=["wav", "mp3"])
 
 if uploaded_file is not None:
-    tfile = NamedTemporaryFile(delete=False) 
-    tfile.write(uploaded_file.read())
-    
-    st.audio(tfile.name, format="audio/wav")
-    texto_transcrito = transcribir_audio(tfile.name)
+    st.audio(uploaded_file, format="audio/wav")
+    texto_transcrito = transcribir_audio(uploaded_file)
     st.write("Texto transcrito:")
     st.write(texto_transcrito)
+    texto_mejorado = mejorar_texto(texto_transcrito)
+    st.write("Texto mejorado:")
+    st.write(texto_mejorado)
