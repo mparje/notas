6dad9a8c02bff19de58fa880f444c4d1f69e11b5
mparje <126131806+mparje@users.noreply.github.com>
2023-04-14 06:56:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,69 +1,48 @@
 import streamlit as st
 import requests
+import os
 
-# URL de la API de Rev AI
-API_URL = "https://api.rev.ai/speechtotext/v1/"
+# Ruta de la API y URL para subir el archivo de audio
+API_URL = "https://api.rev.ai/speechtotext/v1/jobs"
+UPLOAD_URL = "https://www.rev.ai/FTC_Sample_1.mp3"
 
-# Función para enviar la solicitud de transcripción
-def submit_job(access_token, url):
-    headers = {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json'}
-    data = {'media_url': url}
-    response = requests.post(API_URL + 'jobs', headers=headers, json=data)
+# Obtener el token de la API desde una variable de entorno
+ACCESS_TOKEN = os.getenv("REVAI_ACCESS_TOKEN")
+
+# Configuración de la aplicación de Streamlit
+st.title("Transcriptor de audio con Rev AI")
+st.write("Sube un archivo de audio para transcribirlo:")
+
+# Subir archivo de audio
+audio_file = st.file_uploader("Archivo de audio", type=["mp3", "wav", "m4a"])
+
+if audio_file is not None:
+    # Configurar el encabezado para la solicitud de la API
+    headers = {"Authorization": f"Bearer {ACCESS_TOKEN}", "Content-Type": "application/json"}
+
+    # Crear la carga útil para la solicitud de la API
+    payload = {"source_config": {"url": UPLOAD_URL}, "metadata": "Transcripción de audio"}
+
+    # Enviar la solicitud POST a la API para iniciar el trabajo de transcripción
+    response = requests.post(API_URL, headers=headers, json=payload)
     response.raise_for_status()
-    return response.json()['id']
 
-# Función para obtener el estado del trabajo de transcripción
-def get_job_status(access_token, id):
-    headers = {'Authorization': 'Bearer ' + access_token}
-    response = requests.get(API_URL + 'jobs/' + id, headers=headers)
+    # Extraer el ID del trabajo de transcripción de la respuesta JSON
+    job_id = response.json()["id"]
+
+    # Esperar a que el trabajo de transcripción se complete
+    status = "in_progress"
+    while status == "in_progress":
+        response = requests.get(f"{API_URL}/{job_id}", headers=headers)
+        response.raise_for_status()
+        status = response.json()["status"]
+        st.write(f"Estado del trabajo de transcripción: {status}")
+    
+    # Descargar la transcripción en formato JSON
+    response = requests.get(f"{API_URL}/{job_id}/transcript", headers=headers, 
+                            params={"speaker_labels": True, "timestamps": True})
     response.raise_for_status()
-    return response.json()['status']
 
-# Función para obtener la transcripción del trabajo de transcripción completado
-def get_transcript(access_token, id):
-    headers = {'Authorization': 'Bearer ' + access_token, 'Accept': 'application/vnd.rev.transcript.v1.0+json'}
-    response = requests.get(API_URL + 'jobs/' + id + '/transcript', headers=headers)
-    response.raise_for_status()
-    transcript = ''
-    for monologue in response.json()['monologues']:
-        for element in monologue['elements']:
-            if element['type'] == 'text':
-                transcript += element['value'] + ' '
-        transcript += '\n\n'
-    return transcript.strip()
-
-# Configuración de la app de Streamlit
-st.title('Transcripción de Voz con Rev AI')
-st.write('Esta aplicación utiliza la API de Rev AI para transcribir audio.')
-
-# Obtención del Access Token de Rev AI
-access_token = st.text_input('Introduce tu Access Token de Rev AI:')
-if not access_token:
-    st.warning('Por favor, introduce un Access Token válido.')
-    st.stop()
-
-# Obtención de la URL del archivo de audio
-url = st.text_input('Introduce la URL del archivo de audio a transcribir:')
-if not url:
-    st.warning('Por favor, introduce una URL válida.')
-    st.stop()
-
-# Envío de la solicitud de transcripción y obtención de la ID del trabajo
-st.write('Enviando solicitud de transcripción...')
-try:
-    job_id = submit_job(access_token, url)
-except Exception as e:
-    st.error('Se ha producido un error al enviar la solicitud de transcripción:')
-    st.error(str(e))
-    st.stop()
-
-# Espera del trabajo de transcripción y obtención de la transcripción
-st.write('Esperando la finalización del trabajo de transcripción...')
-status = get_job_status(access_token, job_id)
-while status != 'transcribed':
-    status = get_job_status(access_token, job_id)
-transcript = get_transcript(access_token, job_id)
-
-# Mostrando la transcripción
-st.write('Transcripción:')
-st.write(transcript)
+    # Imprimir la transcripción
+    transcript = response.json()
+    st.write(transcript)
