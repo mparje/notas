51141937222f0d8ec36ed43c2cbf576c49c1512d
mparje <126131806+mparje@users.noreply.github.com>
2023-04-14 05:20:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,78 @@
-import streamlit as st
-import requests
-import os
+import base64
 import json
+import os
+import requests
+import srtreamlit as st
+import openai
 
 # Definir la clave de la API de Rev.ai
 rev_api_key = '02l5R8_G98YSnNk_J5iQXhuVMMVo6cKcraDdGz-XrGRKGZUVlSO1PLmst2krtbrprwhZlLSsYhqK4jAhs8XIYQTUShtdY'
 
 # Obtener la API key de OpenAI desde una variable de entorno
 openai_api_key = os.getenv("OPENAI_API_KEY")
+
+
+def transcribe_audio(file):
+    # Leer los datos de audio desde el archivo
+    with open(file, "rb") as f:
+        audio_data = f.read()
+
+    # Convertir los datos de audio a una cadena codificada en base64
+    audio_b64 = base64.b64encode(audio_data).decode()
+
+    # Configurar la URL de la API y los headers
+    url = "https://api.rev.ai/speechtotext/v1/jobs"
+    headers = {
+        "Authorization": f"Bearer {os.getenv('REV_AI_API_KEY')}",
+        "Content-Type": "application/json"
+    }
+
+    # Configurar los parámetros de la solicitud
+    data = {
+        "media": {
+            "type": "audio",
+            "content": audio_b64
+        },
+        "metadata": "Test"
+    }
+
+    # Hacer la solicitud a la API
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    # Leer la respuesta de la API y obtener el ID del trabajo de transcripción
+    response_data = json.loads(response.content)
+    job_id = response_data["id"]
+
+    # Esperar hasta que el trabajo de transcripción esté completo
+    while True:
+        response = requests.get(f"{url}/{job_id}", headers=headers)
+        response_data = json.loads(response.content)
+        status = response_data["status"]
+        if status == "transcribed":
+            break
+        elif status == "failed":
+            raise Exception("La transcripción falló")
+
+        time.sleep(1)
+
+    # Obtener el texto transcrito
+    transcript = response_data["monologues"][0]["elements"]
+    text = " ".join([element["value"] for element in transcript])
+
+    # Ordenar el texto usando el modelo text-davinci-002
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=text,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
+    sorted_text = response.choices[0].text.strip()
+
+    # Devolver el texto transcrito y ordenado
+    return sorted_text
+
 
 # Función para enviar el archivo de audio a la API de Rev.ai y obtener la transcripción
 def transcribe_audio(file):
@@ -82,21 +147,3 @@ st.set_page_config(page_title="Transcripción de Audio", page_icon=":microphone:
 # Título y descripción
 st.title("Transcripción de Audio y Ordenamiento de Texto")
 st.markdown("Esta aplicación usa la API de Rev.ai para transcribir audio y la API de OpenAI para ordenar el texto transcrito.")
-
-# Selección de archivo de audio
-audio_file = st.file_uploader("Selecciona un archivo de audio", type=["mp3", "wav"])
-
-# Si se ha seleccionado un archivo
-if audio_file is not None:
-    # Transcribir el audio
-    st.markdown("Transcribiendo audio...")
-    text = transcribe_audio(audio_file)
-
-    # Mostrar la transcripción
-    st.markdown("## Transcripción")
-    st.write(text)
-
-    # Ordenar el texto
-    st.markdown("Ordenando texto...")
-    sorted_text = sort_text(text)
-
