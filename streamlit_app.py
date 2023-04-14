c6b37438947fa7d8a77a3484627d591bc4bdcc08
mparje <126131806+mparje@users.noreply.github.com>
2023-04-14 07:05:50 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,43 @@
 import streamlit as st
-import os
 import requests
-import json
-
-def transcribe_audio(audio_file, api_key):
-    url = "https://api.amberscript.com/v1/trascription"
-    headers = {
-        "x-api-key": api_key
-    }
-    data = {
-        "language": "es",
-        "audio_url": "",
-    }
-    
-    with open(audio_file, "rb") as file:
-        response = requests.post(url, headers=headers, data=data, files={"audio_file": file})
-    
-    if response.status_code == 200:
-        response_json = json.loads(response.text)
-        return response_json["text"]
-    else:
-        return "Error en la transcripción del archivo de audio."
-
-st.set_page_config(page_title="Transcripción de audio", page_icon=":microphone:", layout="wide")
-
-st.title("Transcripción de audio")
+import os
 
 # Obtener el token de la API desde una variable de entorno
 ACCESS_TOKEN = os.getenv("REVAI_ACCESS_TOKEN")
 
-audio_file = st.file_uploader("Subir archivo de audio", type=["mp3", "wav"])
-if audio_file is not None:
-    result = transcribe_audio(audio_file, api_key)
-    st.subheader("Texto transcrito:")
+st.title("Transcripción de audio con Rev.ai")
+
+# Agregar la opción para subir archivo
+file = st.file_uploader("Seleccionar archivo de audio", type=["mp3", "wav"])
+
+if file:
+    # Configurar la solicitud
+    headers = {
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Content-Type": "multipart/form-data"
+    }
+
+    # Enviar la solicitud para subir el archivo a Rev.ai
+    response = requests.post("https://api.rev.ai/speechtotext/v1beta/sync", headers=headers, data=file.read())
+
+    # Obtener el ID del trabajo de transcripción
+    job_id = response.json()["id"]
+
+    # Configurar la solicitud para obtener el resultado de la transcripción
+    headers["Accept"] = "application/vnd.rev.transcript.v1.0+text"
+    params = {"filter": "transcript"}
+
+    # Esperar hasta que la transcripción esté lista
+    while True:
+        response = requests.get(f"https://api.rev.ai/speechtotext/v1beta/jobs/{job_id}", headers=headers, params=params)
+        status = response.json()["status"]
+        if status == "transcribed":
+            break
+
+    # Obtener el resultado de la transcripción y mostrarlo en la página
+    transcript = response.json()["monologues"][0]["elements"]
+    result = ""
+    for element in transcript:
+        result += element["value"]
+    st.write("Texto transcrito:")
     st.write(result)
