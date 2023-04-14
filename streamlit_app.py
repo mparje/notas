1a672d7da30849332fa6f4b34b71a7a84d10a08b
mparje <126131806+mparje@users.noreply.github.com>
2023-04-14 04:45:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,63 +1,35 @@
 import requests
 import streamlit as st
-import openai
-import os
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+# Configurar la URL y la clave de API de Rev.ai
+url = "https://api.rev.ai/speechtotext/v1/jobs"
+headers = {
+    "Authorization": "Bearer 02l5R8_G98YSnNk_J5iQXhuVMMVo6cKcraDdGz-XrGRKGZUVlSO1PLmst2krtbrprwhZlLSsYhqK4jAhs8XIYQTUShtdY",
+    "Content-Type": "application/json"
+}
 
+# Definir la interfaz de usuario de la aplicación
+st.title("Transcripción de Audio a Texto")
+st.write("Esta aplicación utiliza la API de Rev.ai para transcribir archivos de audio a texto. Introduce la URL del archivo de audio que quieres transcribir:")
 
-# función para mejorar el texto generado
-def mejorar_texto(texto_generado):
-    # completar la oración del usuario para obtener un texto coherente
-    prompt = "Completa la siguiente oración: \"" + texto_generado + "\" "
-    response = openai.Completion.create(
-      engine="text-davinci-003",
-      prompt=prompt,
-      temperature=0.5,
-      max_tokens=60,
-      n=1,
-      stop=None,
-      timeout=15,
-      )
-    # obtener el texto mejorado generado por el modelo de lenguaje
-    texto_mejorado = response.choices[0].text.strip()
-    return texto_mejorado
+audio_url = st.text_input("URL del archivo de audio")
 
-# título de la aplicación
-st.title('Transcripción y mejora de texto')
-
-# obtener la URL del audio a transcribir
-url_audio = st.text_input('Ingrese la URL del audio a transcribir:')
-
-# transcribir el audio utilizando el servicio de reconocimiento de voz de Rev.ai
-if st.button('Transcribir'):
-    headers = {
-        'Authorization': 'Bearer 02l5R8_G98YSnNk_J5iQXhuVMMVo6cKcraDdGz-XrGRKGZUVlSO1PLmst2krtbrprwhZlLSsYhqK4jAhs8XIYQTUShtdY',
-        'Content-Type': 'application/json'
-    }
+# Manejar el envío del formulario
+if st.button("Transcribir"):
+    # Configurar los datos para enviar la solicitud POST a la API de Rev.ai
     data = {
-        'source_config': {
-            'url': url_audio
+        "source_config": {
+            "url": audio_url
         },
-        'metadata': 'Audio de prueba'
+        "metadata": "Transcripción de audio a texto"
     }
-    response = requests.post('https://api.rev.ai/speechtotext/v1/jobs', headers=headers, json=data)
 
+    # Enviar la solicitud POST a la API de Rev.ai
+    response = requests.post(url, headers=headers, json=data)
+
+    # Manejar la respuesta de la API
     if response.status_code == 200:
-        # obtener el resultado de la transcripción del audio
-        resultado = response.json()
-
-        # obtener el texto generado por el servicio de reconocimiento de voz
-        texto_generado = resultado['transcript']
-
-        # mejorar el texto generado utilizando la función mejorado_texto()
-        texto_mejorado = mejorar_texto(texto_generado)
-
-        # mostrar el texto generado y el texto mejorado en la interfaz de usuario
-        st.subheader('Texto generado:')
-        st.write(texto_generado)
-        st.subheader('Texto mejorado:')
-        st.write(texto_mejorado)
-
+        job_id = response.json()["id"]
+        st.write("Se ha iniciado una nueva tarea de transcripción con ID:", job_id)
     else:
-        st.error('¡Ocurrió un error al obtener el resultado de la transcripción del audio!') 
+        st.write("Se ha producido un error al enviar la solicitud. Código de estado:", response.status_code)
