bedcd13d034a04850aaa8192c44d864a4f14f41f
mparje <126131806+mparje@users.noreply.github.com>
2023-04-14 04:59:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,35 @@
-import requests
 import streamlit as st
+import requests
 
-# Configurar la URL y la clave de API de Rev.ai
-url = "https://api.rev.ai/speechtotext/v1/jobs"
-headers = {
-    "Authorization": "Bearer 02l5R8_G98YSnNk_J5iQXhuVMMVo6cKcraDdGz-XrGRKGZUVlSO1PLmst2krtbrprwhZlLSsYhqK4jAhs8XIYQTUShtdY",
-    "Content-Type": "application/json"
-}
+# URL y clave de API de Rev.ai
+API_URL = 'https://api.rev.ai/revspeech/v1beta/jobs'
+API_KEY = '02l5R8_G98YSnNk_J5iQXhuVMMVo6cKcraDdGz-XrGRKGZUVlSO1PLmst2krtbrprwhZlLSsYhqK4jAhs8XIYQTUShtdY'
 
-# Definir la interfaz de usuario de la aplicación
-st.title("Transcripción de Audio a Texto")
-st.write("Esta aplicación utiliza la API de Rev.ai para transcribir archivos de audio a texto. Introduce la URL del archivo de audio que quieres transcribir:")
 
-audio_url = st.text_input("URL del archivo de audio")
 
-# Manejar el envío del formulario
-if st.button("Transcribir"):
-    # Configurar los datos para enviar la solicitud POST a la API de Rev.ai
+# Define la interfaz de usuario de la aplicación
+st.title('Transcripción de Audio a Texto')
+st.write('Esta aplicación utiliza la API de Rev.ai para transcribir archivos de audio a texto. Introduce la URL del archivo de audio que quieres transcribir:')
+
+audio_url = st.text_input('URL del archivo de audio')
+
+# Maneja el envío del formulario
+if st.button('Transcribir'):
+    # Configura los datos para enviar la solicitud POST a la API de Rev.ai
+    headers = {'Authorization': f'Bearer {API_KEY}'}
     data = {
-        "source_config": {
-            "url": audio_url
-        },
-        "metadata": "Transcripción de audio a texto"
+        'media_url': audio_url,
+        'metadata': 'Transcripción de audio a texto',
+        'callback_url': 'http://tu-URL-de-callback-aquí.com'
     }
 
-    # Enviar la solicitud POST a la API de Rev.ai
-    response = requests.post(url, headers=headers, json=data)
+    # Envía la solicitud POST a la API de Rev.ai
+    response = requests.post(API_URL, headers=headers, json=data)
 
-    # Manejar la respuesta de la API
+    # Maneja la respuesta de la API
     if response.status_code == 200:
-        job_id = response.json()["id"]
-        st.write("Se ha iniciado una nueva tarea de transcripción con ID:", job_id)
+        st.write('Se ha iniciado una nueva tarea de transcripción.')
+        job_id = response.json()['id']
+        st.write(f'ID de tarea: {job_id}')
     else:
-        st.write("Se ha producido un error al enviar la solicitud. Código de estado:", response.status_code)
+        st.write('Se ha producido un error al enviar la solicitud.')
