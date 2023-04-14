d57805e9af9d0ee96883d53692db34d475c50d78
mparje <126131806+mparje@users.noreply.github.com>
2023-04-14 05:54:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,78 +1,82 @@
 import streamlit as st
 import requests
-import pandas as pd
 import os
+import json
 import openai
 
-# Definir la clave de la API de Rev.ai
-rev_api_key = os.getenv("REV_API-KEY")
-# Obtener la API key de OpenAI desde una variable de entorno
-openai_api_key = os.getenv("OPENAI_API_KEY")
+# Establecer las credenciales de OpenAI
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Función para transcribir audio usando la API de Rev.ai
-def transcribe_audio(file):
+# Definir la función para transcribir el audio usando Rev.ai
+def transcribe_audio(audio_file):
     url = "https://api.rev.ai/speechtotext/v1/jobs"
     headers = {
-        "Authorization": f"Bearer {os.getenv('REV_AI_API_KEY')}",
-        "Content-Type": "application/json"
+        "Authorization": "Bearer " + os.getenv("REV_AI_API_KEY"),
+        "Content-Type": "application/json",
     }
     data = {
-        "media_url": file,
-        "metadata": "Test"
+        "media": {"type": "multipart/form-data", "data": audio_file},
+        "metadata": "Testing",
     }
     response = requests.post(url, headers=headers, json=data)
     response.raise_for_status()
-    job_id = response.json()["id"]
-    
-    url = f"https://api.rev.ai/speechtotext/v1/jobs/{job_id}/transcript"
-    headers = {
-        "Authorization": f"Bearer {os.getenv('REV_AI_API_KEY')}"
-    }
-    while True:
-        response = requests.get(url, headers=headers)
+    response_data = json.loads(response.text)
+    job_id = response_data["id"]
+    transcript = None
+    while transcript is None:
+        response = requests.get(
+            f"https://api.rev.ai/speechtotext/v1/jobs/{job_id}/transcript",
+            headers=headers,
+        )
         response.raise_for_status()
-        data = response.json()
-        if data["status"] == "in_progress":
-            continue
-        elif data["status"] == "transcribed":
-            return data["monologues"][0]["elements"]
-        else:
-            return None
+        response_data = json.loads(response.text)
+        if response_data["status"] == "transcribed":
+            transcript_url = response_data["links"]["self"]
+            response = requests.get(transcript_url, headers=headers)
+            response.raise_for_status()
+            transcript_data = json.loads(response.text)
+            transcript = transcript_data["monologues"][0]["elements"]
+    return transcript
 
-# Función para ordenar texto usando la API de OpenAI
-def sort_text(transcription):
-    response = openai.Completion.create(
+
+# Definir la función para ordenar el texto
+def order_text(transcript):
+    prompt = " ".join([elem["value"] for elem in transcript])
+    completions = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"Sort the following text in ascending order:\n\n{transcription}",
+        prompt=prompt,
         max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.5,
     )
-    return response.choices[0].text.strip()
-
-# Configuración de la aplicación de Streamlit
-st.set_page_config(page_title="Transcripción y ordenamiento de audio", page_icon=":memo:", layout="wide")
-
-st.title("Transcripción y ordenamiento de audio")
-
-# Subida de archivo
-uploaded_file = st.file_uploader("Subir archivo de audio", type=["mp3", "wav", "flac"])
-
-if uploaded_file is not None:
-    # Transcripción de audio usando la API de Rev.ai
-    st.write("Transcribiendo audio...")
-    elements = transcribe_audio(uploaded_file)
-    transcription = " ".join([element["value"] for element in elements])
-    
-    # Ordenamiento de texto usando la API de OpenAI
-    st.write("Ordenando texto...")
-    sorted_text = sort_text(transcription)
-    
-    # Mostrar resultados
-    st.write("Transcripción:")
-    st.write(transcription)
-    st.write("Texto ordenado:")
-    st.write(sorted_text)
+    message = completions.choices[0].text
+    return message.strip()
 
 
+# Definir la aplicación Streamlit
+def app():
+    st.set_page_config(page_title="Transcribir y ordenar texto de audio")
+    st.title("Transcribir y ordenar texto de audio")
+
+    # Subir el archivo de audio
+    st.header("Subir archivo de audio")
+    audio_file = st.file_uploader("Seleccionar archivo de audio", type=["mp3", "wav"])
+    if audio_file is not None:
+        st.success("Archivo cargado correctamente")
+
+        # Transcribir el audio
+        st.header("Transcribir audio")
+        if st.button("Transcribir"):
+            transcript = transcribe_audio(audio_file)
+            st.write(transcript)
+
+        # Ordenar el texto transcribido
+        st.header("Ordenar texto transcribido")
+        if st.button("Ordenar texto"):
+            message = order_text(transcript)
+            st.write(message)
+
+
+if __name__ == "__main__":
+    app()
