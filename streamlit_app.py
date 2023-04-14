4d01a8cf812d57675ea607a58dca36bacf78c32c
mparje <126131806+mparje@users.noreply.github.com>
2023-04-14 06:48:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,80 +1,70 @@
 import streamlit as st
 import requests
 import json
+import os
+import time
 
-
+# Función para enviar el archivo de audio a la API de Rev.ai
 def transcribe_audio(audio_file):
     url = 'https://api.rev.ai/speechtotext/v1/jobs'
     headers = {
-        'Authorization': f'Bearer {REVAI_ACCESS_TOKEN}',
+        'Authorization': f'Bearer {os.environ["REVAI_ACCESS_TOKEN"]}',
         'Content-Type': 'application/json'
     }
-    data = {
+    response = requests.post(url, headers=headers, data=json.dumps({
         'media_url': audio_file,
         'metadata': 'Streamlit Testing'
-    }
-    response = requests.post(url, headers=headers, json=data)
-
-    if response.status_code != 200:
-        raise ValueError(f'Request failed with error {response.status_code}, {response.text}')
-
+    }))
     job_id = response.json()['id']
-    st.write(f'Job ID: {job_id}')
-
-    url = f'https://api.rev.ai/speechtotext/v1/jobs/{job_id}/transcript'
+    status = None
+    while status != 'transcribed':
+        status_url = f'https://api.rev.ai/speechtotext/v1/jobs/{job_id}/transcript'
+        headers['Accept'] = 'application/vnd.rev.transcript.v1.0+json'
+        response = requests.get(status_url, headers=headers)
+        status = response.json()['status']
+        time.sleep(1)
+    transcript_url = f'https://api.rev.ai/speechtotext/v1/jobs/{job_id}/transcript'
     headers['Accept'] = 'application/vnd.rev.transcript.v1.0+json'
-    response = requests.get(url, headers=headers)
-
-    if response.status_code != 200:
-        raise ValueError(f'Request failed with error {response.status_code}, {response.text}')
-
-    result = response.json()
-    text = ''
-    for monologue in result['monologues']:
-        for element in monologue['elements']:
-            if element['type'] == 'text':
-                text += element['value'] + ' '
-    return text.strip()
+    response = requests.get(transcript_url, headers=headers)
+    return response.json()
 
 
+# Función para ordenar el texto con la API de OpenAI
 def sort_text(text):
     url = 'https://api.openai.com/v1/engines/text-davinci-003/completions'
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': f'Bearer {OPENAI_API_KEY}'
+        'Authorization': f'Bearer {os.environ["OPENAI_API_KEY"]}'
     }
     data = {
         'prompt': text,
         'temperature': 0.5,
-        'max_tokens': 60,
-        'top_p': 1,
-        'frequency_penalty': 0,
-        'presence_penalty': 0
+        'max_tokens': 50,
+        'top_p': 1.0,
+        'frequency_penalty': 0.0,
+        'presence_penalty': 0.0
     }
     response = requests.post(url, headers=headers, json=data)
-    
-    if response.status_code != 200:
-        raise ValueError(f'Request failed with error {response.status_code}, {response.text}')
-    
-    result = response.json()
-    text = result['choices'][0]['text']
-    return text.strip()
+    return response.json()['choices'][0]['text']
 
 
-st.set_page_config(page_title='Transcriber App', page_icon=':memo:', layout='wide')
-st.title('Transcriber App')
-
-# Set your API keys here
-REVAI_ACCESS_TOKEN = '<YOUR_REVAI_ACCESS_TOKEN>'
-OPENAI_API_KEY = '<YOUR_OPENAI_API_KEY>'
-
-audio_file = st.file_uploader('Upload audio file', type=['mp3', 'wav'])
-if audio_file:
-    transcript = transcribe_audio(audio_file)
-    st.header('Transcript')
-    st.write(transcript)
-
-    if st.button('Sort Text'):
-        sorted_text = sort_text(transcript)
-        st.header('Sorted Text')
+# Interfaz de usuario
+def app():
+    st.title("Transcriptor de audio")
+    audio_file = st.file_uploader("Sube un archivo de audio", type=["mp3"])
+    if audio_file:
+        st.audio(audio_file, format='audio/mp3')
+        transcript = transcribe_audio(audio_file)
+        text = ''
+        for monologue in transcript['monologues']:
+            for element in monologue['elements']:
+                if element['type'] == 'text':
+                    text += element['value'] + ' '
+        st.subheader("Texto transcrito:")
+        st.write(text)
+        sorted_text = sort_text(text)
+        st.subheader("Texto ordenado:")
         st.write(sorted_text)
+
+if __name__ == '__main__':
+    app()
