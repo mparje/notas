a93206d6fcd48cddd26351ba4ab6509e9d1782a7
mparje <126131806+mparje@users.noreply.github.com>
2023-04-14 08:20:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import os
 import streamlit as st
 import openai
-import sounddevice as sd
 import soundfile as sf
 
 st.title("App de Speech to Text con OpenAI")
@@ -9,24 +8,31 @@ st.title("App de Speech to Text con OpenAI")
 # Obtener la clave de la API de OpenAI desde una variable de entorno
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-st.write("Haga clic en el botón para empezar a grabar:")
+st.write("Cargue un archivo MP3 para transcribir:")
 
-# Función que graba el audio del usuario y lo guarda en un archivo WAV
-def record_audio():
-    duration = 5  # Duración de la grabación en segundos
-    fs = 44100  # Frecuencia de muestreo del audio
-    recording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
-    sd.wait()
-    filename = "grabacion.wav"
-    sf.write(filename, recording, fs)
-    return filename
+# Función que carga el archivo de audio seleccionado por el usuario
+def load_audio():
+    uploaded_file = st.file_uploader("Seleccione un archivo de audio", type=["mp3"])
+    if uploaded_file is not None:
+        filename = "audio_file.mp3"
+        with open(filename, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        return filename
 
-if st.button("Grabar"):
-    filename = record_audio()
-    st.write("Grabación finalizada. Transcribiendo texto...")
-    
-    # Leer el archivo WAV y enviarlo a la API de OpenAI para obtener la transcripción
+filename = load_audio()
+
+if filename:
+    st.write("Transcribiendo texto...")
+
+    # Leer el archivo MP3 y convertirlo a WAV para poder enviarlo a la API de OpenAI
     with open(filename, "rb") as f:
+        audio_data = f.read()
+    wav_data, sample_rate = sf.read(audio_data, dtype='float32')
+    temp_file = "temp_audio_file.wav"
+    sf.write(temp_file, wav_data, sample_rate)
+
+    # Leer el archivo WAV convertido y enviarlo a la API de OpenAI para obtener la transcripción
+    with open(temp_file, "rb") as f:
         audio_data = f.read()
     response = openai.Completion.create(
         engine="davinci",
@@ -36,7 +42,7 @@ if st.button("Grabar"):
         n_best=1,
         temperature=0,
     )
-    
+
     # Mostrar la transcripción en la página
     transcription = response.choices[0].text.strip()
     st.write("Transcripción:")
